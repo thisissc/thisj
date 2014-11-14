aa35edac7387fe9380f6cd375f7042497d0957fa
thisissc <thisissc@gmail.com>
2014-11-14 18:03:16 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='thisj',
+      version='0.1',
+      description='Python web framework, base on aiohttp',
+      author='Samuel Zhou',
+      author_email='thisissc@gmail.com',
+      url='https://code.google.com/p/thisj/',
+      packages=['thisj'],
+ )
+
